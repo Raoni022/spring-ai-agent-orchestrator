ec133239ff80d863318e29e9de692be5a751bf18
Raoni Medeiros <62862446+Raoni022@users.noreply.github.com>
2026-04-18 16:01:07 -0300

    Add Spring Boot application entry point

---
+++ src/main/java/com/raoni/agentorchestrator/AgentOrchestratorApplication.java
@@ -0,0 +1,11 @@
+package com.raoni.agentorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentOrchestratorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgentOrchestratorApplication.class, args);
+    }
+}
